ecdd779dc89c6d1494ed6635d69941dc346d9f89
franckentien <franckentien@hotmail.fr>
2018-11-02 15:58:29 +0100

    Create Drawer

--- lib/main.dart
+++ lib/main.dart
@@ -17,15 +17,17 @@ class MyApp extends StatelessWidget {
         // "hot reload" (press "r" in the console where you ran "flutter run",
         // or press Run > Flutter Hot Reload in IntelliJ). Notice that the
         // counter didn't reset back to zero; the application is not restarted.
-        primarySwatch: Colors.blue,
+        primaryColor: new Color.fromRGBO(120, 10, 10, 1.0),
+        accentColor: new Color.fromRGBO(0, 77, 22, 1.0)
+            
       ),
-      home: new MyHomePage(title: 'Flutter Demo Home Page'),
+      home: new MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
+  MyHomePage({Key key}) : super(key: key);
 
   // This widget is the home page of your application. It is stateful, meaning
   // that it has a State object (defined below) that contains fields that affect
@@ -36,7 +38,7 @@ class MyHomePage extends StatefulWidget {
   // used by the build method of the State. Fields in a Widget subclass are
   // always marked "final".
 
-  final String title;
+
 
   @override
   _MyHomePageState createState() => new _MyHomePageState();
@@ -68,7 +70,53 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: new AppBar(
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
-        title: new Text(widget.title),
+        title: new Text("Convention Geek"),
+      ),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            DrawerHeader(
+              child: Text('Convention Geek'),
+
+              decoration: BoxDecoration(
+                color: new Color.fromRGBO(120, 10, 10, 1.0),
+              ),
+            ),
+            ListTile(
+              title: Text('Conventions'),
+              onTap: () {
+                // Update the state of the app
+                // ...
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              title: Text('News'),
+              onTap: () {
+                // Update the state of the app
+                // ...
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              title: Text('Annuaire'),
+              onTap: () {
+                // Update the state of the app
+                // ...
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              title: Text('À propos'),
+              onTap: () {
+                // Update the state of the app
+                // ...
+                Navigator.pop(context);
+              },
+            ),
+          ],
+        ),
       ),
       body: new Center(
         // Center is a layout widget. It takes a single child and positions it
