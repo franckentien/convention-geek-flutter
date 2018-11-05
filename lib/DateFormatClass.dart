90efb163839420b263ed41233d6f086781168a68
franckentien <franckentien@hotmail.fr>
2018-11-05 17:37:48 +0100

    Add data in list for index screen

---
+++ lib/DateFormatClass.dart
@@ -0,0 +1,126 @@
+import 'package:intl/intl.dart';
+
+
+class DateFormatClass
+{
+  static getFullMonth(month){
+    String rst="";
+    switch (month){
+      case 1:
+        rst = "janvier";
+        break;
+      case 2:
+        rst = "février";
+        break;
+      case 3:
+        rst = "mars";
+        break;
+      case 4:
+        rst = 'avril';
+        break;
+      case 5:
+        rst = 'mai';
+        break;
+      case 6:
+        rst = "juin";
+        break;
+      case 7:
+        rst = "juillet";
+        break;
+      case 8:
+        rst = "août";
+        break;
+      case 9:
+        rst = 'septembre';
+        break;
+      case 10:
+        rst = 'octobre';
+        break;
+      case 11:
+        rst = 'novembre';
+        break;
+      case 12:
+        rst = 'décembre';
+        break;
+    }
+    return rst;
+  }
+
+  static getShortMonth(month){
+    String rst="";
+    switch (month){
+      case 1:
+        rst = "janv.";
+        break;
+      case 2:
+        rst = "fév.";
+        break;
+      case 3:
+        rst = "mars";
+        break;
+      case 4:
+        rst = 'avril';
+        break;
+      case 5:
+        rst = 'mai';
+        break;
+      case 6:
+        rst = "juin";
+        break;
+      case 7:
+        rst = "juil.";
+        break;
+      case 8:
+        rst = "août";
+        break;
+      case 9:
+        rst = 'sept.';
+        break;
+      case 10:
+        rst = 'oct.';
+        break;
+      case 11:
+        rst = 'nov.';
+        break;
+      case 12:
+        rst = 'déc.';
+        break;
+    }
+    return rst;
+  }
+
+  static getDisplayDate(String dateDebut, String dateFin){
+
+    DateTime datedebut = DateTime.parse(dateDebut).toUtc().add(new Duration(hours: 1));
+    DateTime datefin = DateTime.parse(dateFin).toUtc().add(new Duration(hours: 1));
+    String rst;
+
+    if(datefin == null){ // If the date is not sure
+      if(datedebut.month == 1 && datedebut.day == 1){ //Year Only
+        rst = datedebut.year.toString();
+      }
+      else{ // Year an month
+        rst = getFullMonth(datedebut.month) + " " + datedebut.year;
+      }
+    }
+    else{// Le jour exacte de debut et de fin
+      if(datedebut.year != datefin.year){ //Sur deux ans
+        rst = datedebut.day.toString() + ' ' + getShortMonth(datedebut.month)+ ' ' + datedebut.year.toString()+
+        ' - '  + datefin.day.toString() + ' ' + getShortMonth(datefin.month)+ ' ' + datefin.year.toString();
+      }
+      else if(datedebut.month != datefin.month){//Sur deux mois
+        rst = datedebut.day.toString() + " " + getShortMonth(datedebut.month) + " " +
+              datefin.day.toString() + " " + getShortMonth(datefin.month) + " " + datefin.year.toString();
+      }
+      else if(datedebut.day != datefin.day){//Sur plusieur jours
+        rst = datedebut.day.toString() + " " + datefin.day.toString() + " " + getFullMonth(datefin.month) + " " + datefin.year.toString();
+      }
+      else{//only 1 day
+        rst = datefin.day.toString() + " " + getFullMonth(datefin.month) + " " + datefin.year.toString();
+      }
+    }
+
+    return rst;
+  }
+
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,11 @@
+import 'dart:async';
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:http/http.dart' as http;
+
+import 'DateFormatClass.dart';
 
 void main() => runApp(new MyApp());
 
@@ -38,56 +45,71 @@ class MyHomePage extends StatefulWidget {
   // used by the build method of the State. Fields in a Widget subclass are
   // always marked "final".
 
-
-
   @override
   _MyHomePageState createState() => new _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  List data;
+
+  Future<String> getData() async {
+
+    final response =
+    await http.get(
+        Uri.encodeFull('https://www.convention-geek.fr/api/next-events'),
+        headers: {
+          "Accept": 'application/json'
+        }
+    );
+
+    if (response.statusCode == 200) {
+      // If the call to the server was successful, parse the JSON
+      this.setState((){
+        data = json.decode(response.body);
+      });
+      return "Success!";
+
+    } else {
+      // If that call was not successful, throw an error.
+      throw Exception('Failed to load post');
+    }
   }
 
+  @override
+  void initState(){
+    this.getData();
+  }
+
+
   Widget _buildSuggestions() {
-    return new ListView.builder(
-        // The itemBuilder callback is called once per suggested
-        // word pairing, and places each suggestion into a ListTile
-        // row. For even rows, the function adds a ListTile row for
-        // the word pairing. For odd rows, the function adds a
-        // Divider widget to visually separate the entries. Note that
-        // the divider may be difficult to see on smaller devices.
+    return new ListView.separated(
+
+        itemCount: data == null ? 0 : data.length,
         itemBuilder: (BuildContext _context, int i) {
           // Add a one-pixel-high divider widget before each row
           // in the ListView.
-          if (i.isOdd) {
-            return new Divider();
-          }
-          return _buildRow();
-        }
+            return _buildRow(i);
+        },
+        separatorBuilder: (BuildContext context, int index) => Divider(),
     );
   }
 
-  Widget _buildRow() {
+  Widget _buildRow(int i) {
     return new ListTile(
+
       title: new Text(
-        "Convention XXX"
+        data[i]["name"]
       ),
       subtitle: new Text(
-        'jj-jj mois year'
+          DateFormatClass.getDisplayDate(data[i]["datedebut"], data[i]["datefin"])
+
       ),
+
       trailing: new Text(
-        '-'
+          data[i]["informateur"]
       ),
+
     );
   }
 
@@ -99,18 +121,19 @@ class _MyHomePageState extends State<MyHomePage> {
     // The Flutter framework has been optimized to make rerunning build methods
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
-    return new Scaffold(
+
+     return new Scaffold(
       appBar: new AppBar(
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
-        title: new Text("Convention Geek"),
+        title: new SvgPicture.asset('assets/banniere.svg')
       ),
       drawer: Drawer(
         child: ListView(
           padding: EdgeInsets.zero,
           children: <Widget>[
             DrawerHeader(
-              child: Text('Convention Geek'),
+              child: new SvgPicture.asset('assets/banniere.svg'),
 
               decoration: BoxDecoration(
                 color: new Color.fromRGBO(120, 10, 10, 1.0),
@@ -124,14 +147,14 @@ class _MyHomePageState extends State<MyHomePage> {
                 Navigator.pop(context);
               },
             ),
-            ListTile(
-              title: Text('News'),
-              onTap: () {
-                // Update the state of the app
-                // ...
-                Navigator.pop(context);
-              },
-            ),
+//            ListTile(
+//              title: Text('News'),
+//              onTap: () {
+//                // Update the state of the app
+//                // ...
+//                Navigator.pop(context);
+//              },
+//            ),
             ListTile(
               title: Text('Annuaire'),
               onTap: () {
